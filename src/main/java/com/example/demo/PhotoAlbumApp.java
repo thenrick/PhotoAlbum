62974a2e80fac11a4ed871145859edbf6c120f2b
Tom Henricksen <tom.henricksen@gmail.com>
2018-08-31 12:29:42 -0400

    Refactor the code for readability

--- src/main/java/com/example/demo/PhotoAlbumApp.java
+++ src/main/java/com/example/demo/PhotoAlbumApp.java
@@ -5,20 +5,39 @@ import org.springframework.web.client.RestTemplate;
 public class PhotoAlbumApp {
 
     public static void main(String args[]) {
-    	Photo[] photo = getPhotoArray(args);
-    	displayPhotoInfo(photo);
+    	// Check how many arguments were passed in
+        if(args.length == 0)
+        {
+            System.out.println("Proper Usage is: java PhotoAlbumApp albumId");
+            System.exit(0);
+        }
+        
+        if(isStringInteger(args[0])) {
+        	String albumId = args[0];
+        	Photo[] photos = getPhotoArray(albumId);
+        	displayPhotoInfo(photos);
+        }
     }
 
-	static void displayPhotoInfo(Photo[] photo) {
-		for (Photo pht : photo){
-        	System.out.println("[" + pht.getId() + "]" + pht.getTitle());
+	static void displayPhotoInfo(Photo[] photos) {
+		for (Photo photo : photos){
+        	System.out.println("[" + photo.getId() + "]" + photo.getTitle());
           }
 		
 	}
 
-	static Photo[] getPhotoArray(String[] args) {
+	static Photo[] getPhotoArray(String albumId) {
 		RestTemplate restTemplate = new RestTemplate();
-        Photo[] photo = restTemplate.getForObject("https://jsonplaceholder.typicode.com/photos?albumId=" + args[0], Photo[].class);
-		return photo;
+        Photo[] photos = restTemplate.getForObject("https://jsonplaceholder.typicode.com/photos?albumId=" + albumId, Photo[].class);
+		return photos;
+	}
+	
+	public static boolean isStringInteger(String number ){
+	    try{
+	        Integer.parseInt(number);
+	    }catch(Exception e ){
+	        return false;
+	    }
+	    return true;
 	}
 }
